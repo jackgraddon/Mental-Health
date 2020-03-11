f22514b839980823c60a47a2fb3185f5ed032ed8
YeetbenderDamB <53065389+YeetbenderDamB@users.noreply.github.com>
2020-03-10 19:27:37 -0700

    name

--- script/name.js
+++ script/name.js
@@ -5,7 +5,7 @@ function firstVisit() {
     var person = prompt("Hi! It looks like you are new! Let's get started.\nWhat's your name?", "");
     if (person == null || person == "") {
         alert("I'd love to know your name, but for now, we can skip past that!");
-        title.innerHTML = `Feeling down?`
+        title.innerHTML = `Feeling down?`;
     } else {
         Cookies.set('name', person, {
             expires: 7
