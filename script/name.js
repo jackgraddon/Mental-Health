7f4d950a759bd68317a644ff113f20d81a2eda5c
YeetbenderDamB <53065389+YeetbenderDamB@users.noreply.github.com>
2020-03-10 19:05:30 -0700

    Initial commit

---
+++ script/name.js
@@ -0,0 +1,14 @@
+function firstVisit() {
+    var person = prompt("Hi! It looks like you are new! Let's get started.\nWhat's your name?", "");
+    if (person == null || person == "") {
+        alert("I'd love to know your name, but for now, we can skip past that!");
+    } else {
+        Cookies.set('name', person, {
+            expires: 7
+        })
+    }
+}
+
+const name = Cookies.get('name');
+
+if (!name) firstVisit();
