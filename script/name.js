e02521648dfff4905bab3433c3ec8e65a144650d
YeetbenderDamB <53065389+YeetbenderDamB@users.noreply.github.com>
2020-03-10 21:07:49 -0700

    Words

--- script/name.js
+++ script/name.js
@@ -1,4 +1,5 @@
 let title = document.querySelector('#title');
+let subtitle = document.querySelector('#subtitle');
 const name = Cookies.get('name');
 
 function firstVisit() {
@@ -6,12 +7,14 @@ function firstVisit() {
     if (person == null || person == "") {
         alert("I'd love to know your name, but for now, we can skip past that!");
         title.innerHTML = `Feeling down?`;
+        subtitle.innerHTML = `We'd love to know your name! Click <a class="text-click" onclick="javascript:firstVisit()">here</a> to let us know!`;
         console.log(title.innerHTML);
     } else {
         Cookies.set('name', person, {
             expires: 7
         })
         title.innerHTML = `Feeling down, ${name}?`;
+        subtitle.innerHTML = `Let's fix that!`;
         console.log(title.innerHTML);
     }
 }
@@ -20,5 +23,6 @@ if (!name) {
     firstVisit();
 } else {
     title.innerHTML = `Feeling down, ${name}?`;
+    subtitle.innerHTML = `Let's fix that!`;
     console.log(title.innerHTML);
 }
